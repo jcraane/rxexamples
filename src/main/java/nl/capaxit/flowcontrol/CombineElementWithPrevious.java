a55cb60801f53c9c7facba037f0cf75146f884c4
jcraane <jcraane@gmail.com>
2017-08-03 10:37:57 +0200

    Added CombineElementWithPrevious.java.

---
+++ src/main/java/nl/capaxit/flowcontrol/CombineElementWithPrevious.java
@@ -0,0 +1,15 @@
+package nl.capaxit.flowcontrol;
+
+import rx.Observable;
+
+import java.util.Arrays;
+
+public class CombineElementWithPrevious {
+    public static void main(String[] args) {
+        final Observable<Integer> numbers = Observable.from(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 7));
+        numbers
+                .zipWith(numbers
+                        .skip(1), (f, s) -> Arrays.asList(f, s))
+                .subscribe(System.out::println);
+    }
+}
