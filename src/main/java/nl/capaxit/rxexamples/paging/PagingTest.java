ecec2c23176225bfad6f82be390173c7973789de
Jamie Craane <jcraane@gmail.com>
2018-03-16 08:58:04 +0100

    Extra paging example.

--- src/main/java/nl/capaxit/rxexamples/paging/PagingTest.java
+++ src/main/java/nl/capaxit/rxexamples/paging/PagingTest.java
@@ -4,6 +4,8 @@ import io.reactivex.Observable;
 import io.reactivex.functions.Predicate;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class PagingTest {
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class PagingTest {
         getResultsTillConditionUsingTakeUntil();
         System.out.println("Take");
         getResultsTillConditionUsingTake();
+        System.out.println("Paging");
+        getNextPagesUntilNoMorePagesExist()
+                .subscribe(System.out::println);
     }
 
     private static void getResultsTillConditionUsingTakeUntil() {
@@ -37,5 +42,42 @@ public class PagingTest {
         }
 
         return Observable.fromIterable(Arrays.asList("Valid", "Valid", "Valid"));
+    }
+
+    private static Observable<Result> getNextPagesUntilNoMorePagesExist() {
+        return Observable.range(0, Integer.MAX_VALUE)
+                .concatMap(page -> getResultResponse(page))
+                .takeWhile(result -> !result.result.isEmpty());
+    }
+
+    private static Observable<Result> getResultResponse(final int page) {
+        if (page == 0 || page == 1) {
+            return Observable.just(new Result(Arrays.asList("Valid", "Valid", "Valid"), true));
+        }
+
+        if (page == 2) {
+            return Observable.just(new Result(Arrays.asList("Valid", "Valid", "Valid"), false));
+        }
+
+        return Observable.just(new Result(Collections.emptyList(), false));
+    }
+
+    private static class Result {
+        List<String> result;
+        boolean hasMoreResults;
+
+        public Result(final List<String> result, final boolean hasMoreResults) {
+            this.result = result;
+            this.hasMoreResults = hasMoreResults;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("Result{");
+            sb.append("result=").append(result);
+            sb.append(", hasMoreResults=").append(hasMoreResults);
+            sb.append('}');
+            return sb.toString();
+        }
     }
 }
